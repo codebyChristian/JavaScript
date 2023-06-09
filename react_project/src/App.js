63b7b5e2c50badc2eefae98cf2731db10496a095
Christian Santos <christ.cardoso.santos@gmail.com>
2023-06-08 21:57:49 -0300

    React

---
+++ react_project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React</h1>
+      <h2>Vamos lá!</h2>
+      <p>Para criação do projeto, seque a mesma lógica do HTML, criando tags e colocando o conteúdo nelas.</p>
+      <p>É só o começo!</p>
+    </div>
+  );
+}
+
+export default App;
